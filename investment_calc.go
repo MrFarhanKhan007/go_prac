8960a5af68700b63c7e410f3a68cc3c5118e44f3
mrfarhankhan007 <fk95034@gmail.com>
2026-04-26 21:14:16 +0530

    Completed next lesson

--- investment_calc.go
+++ investment_calc.go
@@ -9,10 +9,13 @@ func investAmountCalculator() {
 	// var investAmount float64 = 1000
 	// var expectedReturnRate = 5.5
 	// var years = 10
-	
+
 	// var investAmount, expexpectedReturnRate float64 = 1000,5.5
 
+	const inflationRate = 2.5
 	investAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
 	futureValue := investAmount * math.Pow(1+expectedReturnRate/100, years)
+	realValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(realValue)
 }
